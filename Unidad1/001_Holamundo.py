14bf19a3951c400bdd1f5daafa93cf39cb98e583
Kevin <za250120159@zapopan.tecmm.edu.mx>
2025-08-30 09:00:49 -0600

    Initial commit

---
+++ Unidad1/001_Holamundo.py
@@ -0,0 +1,19 @@
+print ("Hola python")
+x=5
+print(x)
+a , b = 3, 7
+print(a+b)
+
+# conditionals
+
+a==10 
+
+if a==10:
+    print("Es  10")
+else:
+    print("No es 10")
+
+valor_decimal=10.1513321653
+
+mi_cadena= "Hola mundo"
+
